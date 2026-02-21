8d5c5bf3080484596806544231a0328e2948cdc9
aariisto <115368147+aariisto@users.noreply.github.com>
2026-02-21 14:00:26 +0100

    DockerFile : add Prisma dbPush

--- backend/prisma/migrations/20260218143946_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "urls" (
-    "id" TEXT NOT NULL,
-    "original" TEXT NOT NULL,
-    "clicks" INTEGER NOT NULL DEFAULT 0,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "urls_pkey" PRIMARY KEY ("id")
-);
